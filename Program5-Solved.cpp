2b599c310efe56fb32dcedb17f549555d93685f8
sanjeev756g <skgtm75@gmail.com>
2022-09-16 12:27:27 +0530

    Program5-Solved

---
+++ Program5-Solved.cpp
@@ -0,0 +1,11 @@
+// WAP to calculate the length of String using printf function.
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int x;
+x=printf("Sanjeev Kumar Gautam\n");
+printf("Length of name is %d",x);
+printf("\n");
+return 0;
+}
